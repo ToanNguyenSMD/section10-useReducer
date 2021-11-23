51035c55c03347d9a1783dd8b2f49a094d959236
Toan Nguyen <toan.nguyen@smartdev.com>
2021-11-23 22:48:58 +0700

    use Context

--- src/App.js
+++ src/App.js
@@ -1,49 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import MainHeader from './components/MainHeader/MainHeader';
-import TodoApp from './components/TodoApp/TodoApp';
-import UseContext from './components/UseContext/UseContext'
+import AuthContext from "./components/store/auth-context";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import MainHeader from "./components/MainHeader/MainHeader";
+import TodoApp from "./components/TodoApp/TodoApp";
+import UseContext from "./components/UseContext/UseContext";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const storedUserLoggin = localStorage.getItem('isLoggedIn')
-  // if(storedUserLoggin === '1') {
-  //   setIsLoggedIn(true)
-  // }
-  useEffect(()=> {
-    const storedUserLogin = localStorage.getItem('isLoggedIn')
-    if(storedUserLogin === '1') {
-      setIsLoggedIn(true)
+  
+  useEffect(() => {
+    const storedUserLogin = localStorage.getItem("isLoggedIn");
+    if (storedUserLogin === "1") {
+      setIsLoggedIn(true);
     }
-  },[])
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1')
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn')
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
   return (
     <React.Fragment>
-      {/* <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} /> */}
-      {/* <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main> */}
+      <AuthContext.Provider value={{isLoggedIn: isLoggedIn}}>
+        <MainHeader onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
 
       {/*  HOC O F8 */}
       {/* <TodoApp /> */}
-      <UseContext />
-
-
+      {/* <UseContext /> */}
     </React.Fragment>
   );
 }
